2734a129e57af94afecec2371a2fad8f5ed65c35
RyanMai <hoangmai1070@gmail.com>
2024-02-25 12:34:27 +1100

    added number to roman conversion

---
+++ num-con.py
@@ -0,0 +1,21 @@
+def intToRom(int: int) -> str:
+    ans = ''
+    roman = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+    val = [1000, 500, 100, 50, 10, 5, 1]
+    prefix = [0, 0, 100, 0, 10, 0, 1]
+    remain = int
+    for i, n in enumerate(val):
+        while remain >= n:
+            ans = ans + roman[i]
+            remain = remain - n
+        for index in range(i + 1, len(prefix)): #prefix case
+            num = prefix[index]
+            if (0 < n - remain <= num):
+                ans = ans + roman[index] + roman[i]
+                remain = remain - n + num
+        if remain < n:
+            continue
+        if remain == 0:
+            break
+    return ans
+print(intToRom(984))
